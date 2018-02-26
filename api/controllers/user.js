0b3479e2e5bfe228eee57885eea9cffe8fc0a965
AndrewHanasiro <andrewkhanasiro@gmail.com>
2018-02-26 20:34:08 -0300

    bcrypt include

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,12 +1,50 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.get_all= (req, res, next) => {
-    console.log('getall');
+exports.get_all = (req, res, next) => {
 };
-exports.user_create= (req, res, next) => {};
-exports.user_get= (req, res, next) => {};
-exports.user_update= (req, res, next) => {};
-exports.user_delete= (req, res, next) => {};
+exports.user_create = (req, res, next) => {
+    User.find({email: req.body.email})
+        .exec()
+        .then((user) => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Já existe este e-mail',
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err,
+                        });
+                    } else {
+                        const newUser = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                        });
+                        newUser
+                            .save()
+                            .then((result) => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'Usuario criado',
+                                });
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err,
+                                });
+                            });
+                    }
+                });
+            }
+        });
+};
+exports.user_get = (req, res, next) => { };
+exports.user_update = (req, res, next) => { };
+exports.user_delete = (req, res, next) => { };
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,18 +1,19 @@
+// Importing lib
 const express = require('express');
-const Auth = require('../middleware/jwt');
 
+// Importing files
+const Auth = require('../middleware/jwt');
 const UserController = require('../controllers/user');
 
+// Creating router
 const router = express.router();
 
+// Routes for User
 router.get('/', Auth, UserController.get_all);
-
 router.post('/', UserController.user_create);
-
 router.get('/:usrId', Auth, UserController.user_get);
-
 router.put('/:usrId', Auth, UserController.user_update);
-
 router.delete('/:usrId', Auth, UserController.user_delete);
 
+// Exporting
 module.exports = router;
